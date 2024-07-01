29f742c10b2d274b0a27a16fa8026bf05d7915a2
Jason Palma <jasonpalma16@gmail.com>
2024-07-01 19:22:44 -0400

    Assignment 2: Create World class

--- Assignment_2/main.cpp
+++ Assignment_2/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 
+const unsigned int NORTH_WALL = 0;
+const unsigned int SOUTH_WALL = 10;
+const unsigned int WEST_WALL = 10;
+const unsigned int EAST_WALL = 0;
+enum Orientation { North = 0, South, East, West };
+
 class Point {
 private:
 	int x;
@@ -10,7 +16,7 @@ private:
 public:
 	void setXY(int x, int y) {
 		this->x = x;
-		(*this).y = y;
+		this->y = y;
 	}
 	int getX() const { return x; }
 	int getY() const { return y; }
@@ -20,11 +26,33 @@ public:
 	}
 
 	Point() { x = 0; y = 0; } // Default constructor
-	~Point() { cout << "Destroying point..."; }
+	~Point() { cout << "Destroying point..." << endl; }
 	 
 };
 
+class World {
+private:
+	Point coins[3];
+	
+public:
+	void setCoinLocation(int i, int x, int y) { coins[i].setXY(x, y); }
 
+	void print() const {
+		for (unsigned int i = 0; i < 3; i++) {
+			cout << "Coin " << i + 1 << ": (" << coins[i].getX() <<
+				", " << coins[i].getY() << ")" << endl;
+		}
+	}
+	
+	World() {
+		cout << "Creating world..." << endl;
+		for (unsigned i = 0; i < 3; i++) {
+			coins[i].setXY(0, 0);
+		}
+	}
+	~World() { cout << "Destorying world" << endl; }
+
+};
 
 int main() {
 	
@@ -33,6 +61,11 @@ int main() {
 
 	cout << "x: " << p1.getX() << " y: " << p1.getY() << endl;
 	p1.print();
+
+	World w;
+	w.setCoinLocation(0, 2, 5); // Note: Index starts at 0
+	w.print();
+
 
 	return 0;
 }
